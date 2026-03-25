d48e1f6ef13b5bf0f071639ff2aa574d5882b276
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2026-03-24 19:05:42 -0500

    adelanto lab terraform

--- infra/env/dev.tfvars
+++ infra/env/dev.tfvars
@@ -1,7 +1,12 @@
-prefix   = "lab8"
-location = "eastus"
-vm_count = 2
-admin_username = "student"
-ssh_public_key = "~/.ssh/id_ed25519.pub"
-allow_ssh_from_cidr = "0.0.0.0/0" # Cambia a tu IP/32
-tags = { owner = "alias", course = "ARSW", env = "dev", expires = "2025-12-31" }
+prefix              = "lab6"
+location            = "centralus"
+vm_count            = 2
+admin_username      = "student"
+ssh_public_key      = "~/.ssh/id_ed25519.pub"
+allow_ssh_from_cidr = "186.28.26.89/32"
+tags = {
+  owner   = "AlejandroHenao2572"
+  course  = "ARSW"
+  env     = "dev"
+  expires = "2026-12-31"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,36 @@
-variable "prefix" { type = string }
-variable "location" { type = string }
-variable "vm_count" { type = number }
-variable "admin_username" { type = string }
-variable "ssh_public_key" { type = string }
-variable "allow_ssh_from_cidr" { type = string }
-variable "tags" { type = map(string) }
+variable "prefix" {
+  description = "Prefijo para nombrar recursos"
+  type        = string
+}
+
+variable "location" {
+  description = "Región de Azure"
+  type        = string
+}
+
+variable "vm_count" {
+  description = "Número de VMs"
+  type        = number
+  default     = 2
+}
+
+variable "admin_username" {
+  description = "Usuario administrador"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Ruta a la clave pública SSH"
+  type        = string
+}
+
+variable "allow_ssh_from_cidr" {
+  description = "CIDR permitido para SSH"
+  type        = string
+}
+
+variable "tags" {
+  description = "Etiquetas comunes"
+  type        = map(string)
+  default     = {}
+}
